c218b74cdbc4f45e7c23a17bf3c75247c5d86984
aywar2000 <polamekkartnija@gmail.com>
2023-12-16 04:29:57 +0100

    zez

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const galebJedan = document.getElementById('galeb-jedan')
 const galebDva = document.getElementById('galeb-dva')
 
+const parallaxWrap = document.querySelector('.parallax-wrapper')
+const brodEl = document.getElementById('brod')
+
 galebJedan.addEventListener('click', ()=>{
     galebDva.classList.add('active')
+    parallaxWrap.classList.add('zoom')
+    brodEl.classList.add('move-left')
 })
