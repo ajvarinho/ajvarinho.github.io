f55cb4f34e4cb2535f19122c964a3ece80ca28b5
aywar2000 <polamekkartnija@gmail.com>
2024-01-05 11:52:05 +0100

    added uc

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const galebJedan = document.getElementById('galeb-jedan')
-const galebDva = document.getElementById('galeb-dva')
+const msgWrap = document.querySelector("#msg-wrap");
+const constructionWrap = document.querySelector(".under-construction");
+const parallaxWrap = document.querySelector(".parallax-wrapper");
 
-const parallaxWrap = document.querySelector('.parallax-wrapper')
-const brodEl = document.getElementById('brod')
-
-galebJedan.addEventListener('click', ()=>{
-    galebDva.classList.add('active')
-    parallaxWrap.classList.add('zoom')
-    brodEl.classList.add('move-left')
-})
+msgWrap.addEventListener("click", () => {
+  console.log("click", constructionWrap);
+  constructionWrap.classList.add("hide");
+});
