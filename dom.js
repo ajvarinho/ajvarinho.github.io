8e07db396d848b941638e7270fe5aba3277580b7
nidjasim <polamekkartnija@gmail.com>
2024-11-13 20:20:36 +0100

    header

--- dom.js
+++ dom.js
@@ -1,9 +1,11 @@
 console.log("djesba");
 
 const openAccessibilityMenu = document.getElementById('a11y-menu')
+const accessibilityWrap = document.querySelector('.a11y-menu')
 
 openAccessibilityMenu.addEventListener('click', ()=>{
   console.log('alo')
+  accessibilityWrap.classList.toggle('active')
 })
 
 
