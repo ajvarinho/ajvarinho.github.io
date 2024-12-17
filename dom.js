ce5e04191a6fdf21750b9ad518e5c99b3ac338bb
nidjasim <polamekkartnija@gmail.com>
2024-12-17 01:51:31 +0100

    test_svg

--- dom.js
+++ dom.js
@@ -1,17 +1,27 @@
-console.log("djesba");
+// console.log("djesba");
 
-const openAccessibilityMenu = document.getElementById('a11y-menu')
-const accessibilityWrap = document.querySelector('.a11y-menu')
+// const openAccessibilityMenu = document.getElementById('a11y-menu')
+// const accessibilityWrap = document.querySelector('.a11y-menu')
 
-openAccessibilityMenu.addEventListener('click', ()=>{
-  console.log('alo')
-  accessibilityWrap.classList.toggle('active')
-})
+// openAccessibilityMenu.addEventListener('click', ()=>{
+//   console.log('alo')
+//   accessibilityWrap.classList.toggle('active')
+// })
 
 
 
-const smallFont = document.getElementById('small')
+// const smallFont = document.getElementById('small')
 
-smallFont.addEventListener('change', ()=>{
-  console.log('change')
+// smallFont.addEventListener('change', ()=>{
+//   console.log('change')
+// })
+
+const blob = document.querySelector('.blob');
+const container = document.querySelector('.container');
+
+blob.addEventListener('click', ()=>{
+	console.log('alo ba')
+	let newBlob = document.createElement('div');
+	newBlob.setAttribute('class', 'blob');
+	container.appendChild(newBlob);
 })
